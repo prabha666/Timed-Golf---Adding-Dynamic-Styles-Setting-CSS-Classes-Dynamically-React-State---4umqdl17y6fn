5a0d97cdd8af25c648b1207c4ab2c6b0acbbde6c
prabha666 <108609740+prabha666@users.noreply.github.com>
2022-09-04 10:07:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,118 +1,100 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-class Timer extends React.Component {
+class App extends React.Component {
+
   constructor(props) {
     super(props);
-    this.id = 0;
-    this.btn = false;
-    this.state = {
-      isGameActive: true,
-      time: 0,
-      x: 0,
+    this.state={
+      renderBall:false, 
+      time: 0, 
+      x: 0, 
       y: 0,
+      top:0,
+      left:0,
+      srartTime:0,
     };
-    this.ballPosition = {
-      left: this.state.x + "px",
-      top: this.state.y + "px",
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.runTimer = this.runTimer.bind(this);
-    this.btnRef = React.createRef(null);
+    this.buttonClickHandler=this.buttonClickHandler.bind(this);
+    this.handeleventlistner=this.handeleventlistner.bind(this);
+    this.tick=this.tick.bind(this);
   }
 
-  handleClick(event) {
-    switch (event.keyCode) {
-      case 37:
-        //left arrow key
-        // console.log(event.keyCode)
-        this.setState((prevState) => {
-          return { ...prevState, x: this.state.x - 5 };
-        });
-        // console.log(this.ballPosition,this.state.x)
-        break;
-      case 38:
-        //top arrow key
-        this.setState((prevState) => {
-          return { ...prevState, y: this.state.y - 5 };
-        });
-        break;
-      case 39:
-        //right arrow key;
-        this.setState((prevState) => {
-          return { ...prevState, x: this.state.x + 5 };
-        });
-        break;
-      case 40:
-        //down arrow key
-        this.setState((prevState) => {
-          return { ...prevState, y: this.state.y + 5 };
-        });
-        break;
-      default:
-        break;
-    }
-  }
-  componentDidMount() {}
-  componentDidUpdate(prevProp, prevState, ss) {
-    // console.log("componentdid update",this.state);
-    if (this.state.x === 250 && this.state.y === 250) {
-      clearInterval(this.id);
-      document.removeEventListener("keydown", this.handleClick);
-      if (this.state.isGameActive) {
-        this.setState({
-          isGameActive: false,
-        });
-      }
-    }
-  }
-
-  componentWillUnmount() {}
-
-  runTimer() {
-    if (!this.btn) {
-      this.btn = true;
-      document.addEventListener("keydown", this.handleClick);
-      this.id = setInterval(() => {
-        this.setState((prevState) => {
-          return { ...prevState, time: this.state.time + 1 };
-        });
-      }, 1000);
-      // console.log("hello",this.id)
-    }
-    this.btnRef.current.disabled = true;
-  }
-
-  render() {
-    return (
-      <>
-        <div
-          style={{
-            left: this.state.x + "px",
-            top: this.state.y + "px",
-          }}
-          className="ball"
-        ></div>
-        <button
-          ref={this.btnRef}
-          onClick={this.runTimer}
-          style={{ position: "absolute", top: 400, left: 300 }}
-          className="start"
-        >
-          Start
-        </button>
-        <div className="heading-timer">Timer: {this.state.time}</div>
-        <div className="hole"></div>
-        <h1
-          className="msg"
-          style={{ position: "relative", left: "40%", top: "300px" }}
-        >
-          {this.state.isGameActive === false
-            ? `Game Ends in ${this.state.time} sec.`
-            : null}
-        </h1>
-      </>
+  buttonClickHandler(){
+    document.addEventListener("keydown",this.handeleventlistner);
+    clearInterval(this.timerID);
+    this.setState({renderBall:true, time: 0, x: 0, y: 0 ,top:0,left:0,startTime:Date.now()})
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
     );
+  }
+
+  tick() {
+    if(!(this.state.x ==250 &&this.state.y==250)){
+      let timePassed= Date.now() - this.state.startTime ;
+      let sec=Math.floor(timePassed/(1000));
+      this.setState({
+      time:sec
+      });
+    }
+  }
+
+  handeleventlistner(e){
+    let code=e.keyCode;
+    
+    if(code==39 && !(this.state.x ==250 &&this.state.y==250)){
+        this.setState({
+            x: this.state.left+5, y: this.state.top ,top:this.state.top,left:this.state.left+5
+        });
+    }
+    if(code==37  && !(this.state.x ==250 &&this.state.y==250)){
+        
+        this.setState({
+          
+         x: this.state.left-5, y: this.state.top ,top:this.state.top,left:this.state.left-5
+      });
+    }
+    if(code==38 && !(this.state.x ==250 &&this.state.y==250)){
+        this.setState({
+          
+          x: this.state.left, y:this.state.top-5 ,top:this.state.top-5,left:this.state.left
+      });
+    }
+    if(code==40 && !(this.state.x ==250 &&this.state.y==250)){
+        this.setState({
+          
+          x:this.state.left, y: this.state.top+5 ,top:this.state.top+5,left:this.state.left
+      });
+    }
+    if(this.state.x==250 && this.state.y==250){
+      alert(this.state.time);
+      clearInterval(this.timerID);
+      this.setState({renderBall:false, time: 0, x: 0, y: 0 ,top:0,left:0,startTime:0})
+      document.removeEventListener("keydown",this.handeleventlistner);
+    }
+  }
+
+    renderBallOrButton(){
+    if (this.state.renderBall) {
+        return (  
+          <>
+          <div className="ball" style={{ position:"absolute",top:this.state.top +"px",
+          left:this.state.left +"px",}}></div>
+          <div className="hole" ></div>
+          <div className="heading-timer">{this.state.time}</div>
+        </>
+        );
+    } 
+    else
+        return <button onClick={this.buttonClickHandler} className="start">Start</button>
+  }
+
+  render(){
+    return (
+      <div>
+          {this.renderBallOrButton()}
+      </div>
+    )
   }
 }
 
-export default Timer;
+export default App;
